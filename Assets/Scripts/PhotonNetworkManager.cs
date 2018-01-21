8ee65b8a815fb2ce61dc163103f1f3bead6661ec
Sam Chai <sam.chai0501@gmail.com>
2018-01-21 13:10:49 -0500

    add photon networking

---
+++ Assets/Scripts/PhotonNetworkManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonNetworkManager : MonoBehaviour {
+
+	[SerializeField] private GameObject player;
+	[SerializeField] private Transform spawnPoint;
+	[SerializeField] private GameObject lobbyCamera;
+	// Use this for initialization
+	void Start () {
+		PhotonNetwork.ConnectUsingSettings("A");
+	}
+
+	public virtual void OnJoinedLobby()
+	{
+		Debug.Log ("we have now joined the lobby");
+
+		PhotonNetwork.JoinOrCreateRoom("NewRoom", null, null);
+	}
+
+	public virtual void OnJoinedRoom()
+	{
+		PhotonNetwork.Instantiate (player.name, spawnPoint.position, spawnPoint.rotation, 0);
+		lobbyCamera.SetActive (false);
+	}
+}
